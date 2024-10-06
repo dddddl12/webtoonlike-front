67e9fb560211fbe4be05c95e6c1e476ef099fcb7
d <fix@github.com>
2024-10-07 00:00:24 +0900

    Refactor AdminRouter to simplify tab rendering logic
    Removed redundant declarations and integrated directly into the return statement. This refactor enhances code readability and reduces unnecessary lines, improving maintainability.

--- src/$pages/AdminPage/router.tsx
+++ src/$pages/AdminPage/router.tsx
@@ -7,27 +7,28 @@ import { ManageBidRoundTab } from "./tabs/ManageBidRoundTab";
 import { ManageUserTab } from "./tabs/ManageUserTab";
 import { Dashboard } from "./tabs/Dashboard";
 import { ManageAdmin } from "./tabs/ManageAdmin";
-import { Col, Container } from "@/ui/layouts";
+import { Container } from "@/ui/layouts";
 import { Invoice } from "./tabs/Invoice";
 import { ManageGenre } from "./tabs/ManageGenre";
 import { ManageSubmitTab } from "./ManageSubmitTab";
 
 
+const tabComponents: { [key: string]: JSX.Element } = {
+  "dashboard": <Dashboard/>,
+  "manage-user": <ManageUserTab/>,
+  "manage-admin": <ManageAdmin/>,
+  "manage-manuscript": <ManageBidRoundTab />,
+  "manage-submit": <ManageSubmitTab />,
+  "manage-offer": <ManageBidRoundRequestTab/>,
+  "manage-invoice": <Invoice/>,
+  "manage-genre": <ManageGenre/>,
+};
+
 export function AdminRouter() {
   const param = useParams();
 
-  const tab = param.tab as string;
-
-  const tabComponents: { [key: string]: JSX.Element } = {
-    "dashboard": <Dashboard/>,
-    "manage-user": <ManageUserTab/>,
-    "manage-admin": <ManageAdmin/>,
-    "manage-manuscript": <ManageBidRoundTab />,
-    "manage-submit": <ManageSubmitTab />,
-    "manage-offer": <ManageBidRoundRequestTab/>,
-    "manage-invoice": <Invoice/>,
-    "manage-genre": <ManageGenre/>,
-  };
-
-  return (<Container className="bg-gray-light">{tabComponents[tab]}</Container> || <div>no tab matching</div>);
+  const tab = tabComponents[param.tab as string];
+  return tab
+    ? <Container className="bg-gray-light">{tab}</Container>
+    : <div>no tab matching</div>;
 }
