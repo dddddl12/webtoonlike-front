a1446bc50355b60223b1a928c962d55a33252131
d <170926579+dddddl12@users.noreply.github.com>
2024-11-11 16:57:11 +0900

    Remove environment condition for global Prisma instance
    The condition to only assign `prisma` to `globalForPrisma` in non-production environments has been removed. This change ensures that the Prisma instance is assigned globally in all environments.

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -16,6 +16,7 @@ export const prisma = globalForPrisma.prisma || new PrismaClient({
   // log: ["query", "info", "warn", "error"],
 });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// if (process.env.NODE_ENV !== "production")
+globalForPrisma.prisma = prisma;
 
 export default prisma;
